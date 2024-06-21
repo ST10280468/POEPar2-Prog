5bb8c67c8c9ce0bb4de3b1ccc6779f842720f566
ST10280468 <ST10280468@vcconnect.edu.za>
2024-06-21 23:29:19 +0200

    first commit

--- test/part3poe/LoginTest.java
+++ test/part3poe/LoginTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
-package part2poe;
+package part3poe;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -24,7 +24,6 @@ public class LoginTest {
         System.out.println("main");
         String[] args = null;
         Login.main(args);
-        
     }
 
     /**
@@ -34,34 +33,24 @@ public class LoginTest {
     public void testPromptUserForLoginDeets() {
         System.out.println("promptUserForLoginDeets");
         Login.promptUserForLoginDeets();
-       
     }
 
     /**
-     * Test of login method, of class Login.
+     * Test of checkUsername method, of class Login.
      */
     @Test
-    public void testLogin() {
-        System.out.println("login");
-        Login.login();
-      
-    }
-
-    /**
-     * Test of loginUser method, of class Login.
-     */
-    @Test
-    public void testLoginUser() {
-        System.out.println("loginUser");
-        String name = "kyle";
+    public void testCheckUsername() {
+        System.out.println("checkUsername");
         String username = "kyl_1";
-        String studentNumber = "ST10280468";
-        boolean expResult = false;
-        boolean result = Login.loginUser(name, studentNumber);
+        boolean expResult = true;
+        boolean result = Login.checkUsername(username);
         assertEquals(expResult, result);
         
+        username = "kyle!!!!!!!";
+        expResult = false;
+        result = Login.checkUsername(username);
+        assertEquals(expResult, result);
     }
-    
 
     /**
      * Test of checkPasswordComplexity method, of class Login.
@@ -70,10 +59,32 @@ public class LoginTest {
     public void testCheckPasswordComplexity() {
         System.out.println("checkPasswordComplexity");
         String password = "Ch&&sec@ke99";
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = Login.checkPasswordComplexity(password);
         assertEquals(expResult, result);
-      
+        
+        password = "password";
+        expResult = false;
+        result = Login.checkPasswordComplexity(password);
+        assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+        System.out.println("loginUser");
+        String name = "kylebrown";
+        String studentNumber = "123456789";
+        boolean expResult = true;
+        boolean result = Login.loginUser(name, studentNumber);
+        assertEquals(expResult, result);
+        
+        name = "kyle";
+        studentNumber = "12345678";
+        expResult = false;
+        result = Login.loginUser(name, studentNumber);
+        assertEquals(expResult, result);
+    }
 }
